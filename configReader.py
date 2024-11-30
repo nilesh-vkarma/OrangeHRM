5030c75f432ce42bd3e262003b6a785af8676d2a
NileshSDET <niljangid2015@gmail.com>
2024-11-30 20:14:38 +0530

    Update configReader.py

--- configReader.py
+++ configReader.py
@@ -4,7 +4,7 @@ import configparser
 class ConfigReader:
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read(r"C:\Users\nilja\PycharmProjects\OrangeHRM\config.properties")
+        self.config.read("add absolute path of config.properties file")
 
     def get_value(self, section, key):
         return self.config.get(section, key)
